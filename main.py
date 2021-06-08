58453924dd855d24bece7ed21804a91bb563b433
Kawdrin <kawan.inf@gmail.com>
2021-06-08 18:11:03 -0300

    Adicionando GUI - Kivy e KivyMD

---
+++ main.py
@@ -0,0 +1,39 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivymd.uix.card import MDCard, MDCardSwipe
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.list import MDList
+from kivymd.uix.tab import MDTabsBase
+
+Window.size = (480, 620)
+
+class AddReceita(MDCard):
+    ...
+
+class CardReceita(MDCardSwipe):
+    ...
+
+class ReceitaInformacoes(MDScreen):
+    ...
+
+class TabelaInformacoes(MDFloatLayout, MDTabsBase):
+    ...
+
+class ListaReceitas(MDList):
+    def adicionar_receita(self):
+        self.add_widget(CardReceita())
+
+class TelaPrincipal(MDFloatLayout):
+    def adicionar_receita(self):
+        self.add_widget(AddReceita())
+
+class AssaCuca(MDApp):
+    def build(self):
+        self.receita_texto = ''
+        self.theme_cls.primary_palette = "Pink"
+        return Builder.load_file("main.kv")
+
+AssaCuca().run()
